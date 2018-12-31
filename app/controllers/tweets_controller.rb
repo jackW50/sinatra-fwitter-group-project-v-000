ff2745ed4826aebed1cb3a7caa836aec2b8848b8
John Werenski <jtwerenski@gmail.com>
2018-12-31 19:43:52 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,73 @@
 class TweetsController < ApplicationController
-
+  
+  get '/tweets' do
+    #binding.pry
+    if logged_in?
+      @tweets = Tweet.all
+      user = current_user 
+      erb :"/tweets/tweets"
+    else 
+      redirect "/login"
+    end 
+  end 
+  
+  get '/tweets/new' do
+    #binding.pry 
+    if logged_in? 
+      erb :"/tweets/new"
+    else 
+      redirect "/login"
+    end 
+  end 
+  
+  post '/tweets' do
+    #binding.pry 
+    tweet = Tweet.new(params)
+    tweet.user = current_user 
+    if tweet.save
+      redirect "/tweets/#{tweet.id}"
+    else 
+      redirect "/tweets/new"
+    end 
+  end 
+  
+  get '/tweets/:id' do
+    if logged_in?
+      @tweet = Tweet.find(params[:id])
+      erb :"/tweets/show_tweet"
+    else 
+      redirect :"/login"
+    end 
+  end 
+  
+  get '/tweets/:id/edit' do
+    @tweet = Tweet.find(params[:id])
+    if @tweet.user_id == current_user.id && logged_in?
+      erb :"/tweets/edit_tweet"
+    else 
+      redirect "/login"
+    end 
+  end 
+  
+  patch '/tweets/:id' do
+    #binding.pry 
+    tweet = Tweet.find(params[:id])
+    if tweet.user_id == current_user.id 
+      tweet.update(content: params[:content])
+      redirect "/tweets/#{tweet.id}"
+    else 
+      redirect "/login"
+    end 
+  end 
+  
+  delete '/tweets/:id/delete' do 
+    tweet = Tweet.find(params[:id])
+    if tweet.user_id == current_user.id && logged_in?
+      tweet.destroy 
+      redirect "/tweets"
+    else 
+      redirect "/login"
+    end 
+  end 
 
 end
