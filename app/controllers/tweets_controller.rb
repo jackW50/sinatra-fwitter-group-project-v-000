88cc0088c09f529594b436248d33971413ee6513
John Werenski <jtwerenski@gmail.com>
2018-12-31 20:23:44 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -58,6 +58,10 @@ class TweetsController < ApplicationController
     elsif logged_in? && current_user.id == tweet.user_id && params[:content]
       redirect "/tweets/#{tweet.id}/edit"
     elsif logged_in? && current_user.id != tweet.user_id && params[:content]
+      redirect "/tweets"
+    else 
+      redirect "/login"
+    end 
       if current_user.id == tweet.user_id && !params[:content].empty?
         tweet.update(content: params[:content])
         redirect "/tweets/#{tweet.id}"
