e1cba09ec7173951ba9793487f2d42fc60e02b6c
John Werenski <jtwerenski@gmail.com>
2018-12-31 20:24:56 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -51,7 +51,7 @@ class TweetsController < ApplicationController
   
   patch '/tweets/:id' do
     #binding.pry 
-    tweet = Tweet.find(params[:id])
+    tweet = Tweet.find_by(content: params[:content])
     if logged_in? && current_user.id == tweet.user_id && !params[:content]
       tweet.update(content: params[:content])
       redirect "/tweets/#{tweet.id}"
