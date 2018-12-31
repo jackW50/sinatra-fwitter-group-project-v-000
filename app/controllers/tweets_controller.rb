aaaf76063ab2dc386792efbb83a638792ae5fd9f
John Werenski <jtwerenski@gmail.com>
2018-12-31 20:43:13 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -40,14 +40,6 @@ class TweetsController < ApplicationController
     end 
   end 
   
-  context "logged out" do
-      it 'does not load -- instead redirects to login' do
-        get '/tweets/1/edit'
-        expect(last_response.location).to include("/login")
-      end
-    end
-  end
-  
   get '/tweets/:id/edit' do
     if logged_in?
       @tweet = Tweet.find(params[:id])
