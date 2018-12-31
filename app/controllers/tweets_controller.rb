83d07d0f38cba60ab62a1e33db57a8449cccf4b8
John Werenski <jtwerenski@gmail.com>
2018-12-31 20:22:52 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -52,8 +52,11 @@ class TweetsController < ApplicationController
   patch '/tweets/:id' do
     #binding.pry 
     tweet = Tweet.find(params[:id])
-    if logged_in && current_user.id == tweet.user_id && !params[:content]
+    if logged_in? && current_user.id == tweet.user_id && !params[:content]
       tweet.update(content: params[:content])
+      redirect "/tweets/#{tweet.id}"
+    elsif logged_in? && current_user.id == tweet.user_id && params[:content]
+      redirect "/tweets/#{tweet.id}/edit"
       if current_user.id == tweet.user_id && !params[:content].empty?
         tweet.update(content: params[:content])
         redirect "/tweets/#{tweet.id}"
